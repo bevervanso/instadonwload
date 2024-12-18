e5a156afaee9c2e3696a6b95c24a0d49d984041e
bevervanso <murilobevervansouk@gmail.com>
2024-12-18 14:58:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# instadonwload
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/bevervanso/instadonwload)
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
+});
